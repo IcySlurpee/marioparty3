264488bbc2358707a8915019556a434327b16f48
IcySlurpee <1784495+IcySlurpee@users.noreply.github.com>
2025-12-06 20:43:09 -0800

    decomp some game-guy shared funcs

--- src/overlays/ovl_42_game_guys_roulette/2FF5F0.c
+++ src/overlays/ovl_42_game_guys_roulette/2FF5F0.c
@@ -1,5 +1,7 @@
 #include "ovl_42.h"
 
+void func_8010A1FC_300ACC_game_guys_roulette(s32, s16, s16, s16, s16);
+
 INCLUDE_ASM("asm/nonmatchings/overlays/ovl_42_game_guys_roulette/2FF5F0", func_80108D20_2FF5F0_game_guys_roulette);
 
 INCLUDE_ASM("asm/nonmatchings/overlays/ovl_42_game_guys_roulette/2FF5F0", func_80108FEC_2FF8BC_game_guys_roulette);
@@ -10,7 +12,21 @@ INCLUDE_ASM("asm/nonmatchings/overlays/ovl_42_game_guys_roulette/2FF5F0", func_8
 
 INCLUDE_ASM("asm/nonmatchings/overlays/ovl_42_game_guys_roulette/2FF5F0", func_80109D14_3005E4_game_guys_roulette);
 
-INCLUDE_ASM("asm/nonmatchings/overlays/ovl_42_game_guys_roulette/2FF5F0", func_80109D98_300668_game_guys_roulette);
+// INCLUDE_ASM("asm/nonmatchings/overlays/ovl_42_game_guys_roulette/2FF5F0", func_80109D98_300668_game_guys_roulette);
+s16 func_80109D98_300668_game_guys_roulette(s32 arg0, s32 arg1, u16 arg2, u16 arg3) {
+    s16 temp_s1;
+    s16 temp_v0;
+
+    temp_s1 = func_8000B838_C438(arg0);
+    temp_v0 = HuSprGrpCreate(1, 0);
+    func_80055024_55C24(temp_v0, 0, temp_s1, 0);
+    HuSprAttrReset(temp_v0, 0, 0xFFFF);
+    HuSprAttrSet(temp_v0, 0, arg1 | 0x1000);
+    HuSprPriSet(temp_v0, 0, arg2);
+    func_80054904_55504(temp_v0, 0, 0xA0, 0x78);
+    func_80055458_56058(temp_v0, 0, arg3);
+    return temp_v0;
+}
 
 INCLUDE_ASM("asm/nonmatchings/overlays/ovl_42_game_guys_roulette/2FF5F0", func_80109E6C_30073C_game_guys_roulette);
 
@@ -18,7 +34,10 @@ INCLUDE_ASM("asm/nonmatchings/overlays/ovl_42_game_guys_roulette/2FF5F0", func_8
 
 INCLUDE_ASM("asm/nonmatchings/overlays/ovl_42_game_guys_roulette/2FF5F0", func_8010A060_300930_game_guys_roulette);
 
-INCLUDE_ASM("asm/nonmatchings/overlays/ovl_42_game_guys_roulette/2FF5F0", func_8010A1BC_300A8C_game_guys_roulette);
+// INCLUDE_ASM("asm/nonmatchings/overlays/ovl_42_game_guys_roulette/2FF5F0", func_8010A1BC_300A8C_game_guys_roulette);
+void func_8010A1BC_300A8C_game_guys_roulette(s16 arg0, s16 arg1, s16 arg2, s16 arg3) {
+    func_8010A1FC_300ACC_game_guys_roulette(arg0, arg1, arg2, 0x2D, arg3);
+}
 
 INCLUDE_ASM("asm/nonmatchings/overlays/ovl_42_game_guys_roulette/2FF5F0", func_8010A1FC_300ACC_game_guys_roulette);
 
